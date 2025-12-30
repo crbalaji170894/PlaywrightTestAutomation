a6c6c14f119afb9489c5761b74dce4c7a1098104
Your Name <you@example.com>
2025-12-30 12:44:16 +0530

    test

--- playwright.config.js
+++ playwright.config.js
@@ -16,7 +16,7 @@ import { trace } from 'node:console';
 const config = ({
   
   testDir: './tests',
-  testMatch: '/Day11-ScreenShotAndTrace.spec.js',
+  testMatch: '/Day12-E2EWebAutomationProject.spec.js',
   timeout: 40 *1000,
 
   expect:{
