334958788426ccaf7d1c9c9424066d06dce1af14
Your Name <you@example.com>
2026-01-01 07:17:46 +0530

    test

---
+++ tests/Day27-Screenshots.spec.js
@@ -0,0 +1,28 @@
+const{test, expect} = require ('@playwright/test');
+
+
+test("locating the webelements", async ({page})=>{
+
+    await page.goto("https://www.facebook.com/");
+
+    await expect(page).toHaveTitle("Facebook – log in or sign up");
+
+    await page.locator('#email').fill("crbalaji17@gmail.com");
+
+    await page.locator('#pass').fill("test@123A");
+    
+    await page.locator ("[name='login']").screenshot({path:'loginlocator.png'});
+
+    await page.locator("[name='login']").click();
+
+    //console.log( await page.locator('._9ay7').textContent());
+
+    await page.screenshot({path:'fulllpage.png'});
+
+    await page.pause();
+
+    //textConet() to extract the text value
+
+    // Scrreshot we can take page level and locator level
+
+});
